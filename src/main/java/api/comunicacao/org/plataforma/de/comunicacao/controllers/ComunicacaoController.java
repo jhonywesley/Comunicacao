37da23a490ce2d88a753021b3e88b8c93cae2049
jhony wesley marques <jhonywesley601@gmail.com>
2020-11-13 14:40:55 -0300

    importacoes do controller de cominicacao

--- src/main/java/api/comunicacao/org/plataforma/de/comunicacao/controllers/ComunicacaoController.java
+++ src/main/java/api/comunicacao/org/plataforma/de/comunicacao/controllers/ComunicacaoController.java
@@ -1,5 +1,16 @@
 package api.comunicacao.org.plataforma.de.comunicacao.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import api.comunicacao.org.plataforma.de.comunicacao.models.Mensagem;
+import api.comunicacao.org.plataforma.de.comunicacao.repository.MensagemRepository;
+
 @RestController
 public class ComunicacaoController {
 
